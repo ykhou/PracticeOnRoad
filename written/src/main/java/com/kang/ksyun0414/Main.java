b6dbf33617e8635ba6bebb61b08eeb94956cc9ab
ykhou <houyongkang@bupt.edu.cn>
2022-04-14 22:24:32 +0800

    金山云

---
+++ written/src/main/java/com/kang/ksyun0414/Main.java
@@ -0,0 +1,16 @@
+package com.kang.ksyun0414;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+//        str = str.toLowerCase();
+        String[] strArray = str.split(" ");
+        System.out.print(strArray[0]);
+        for (int i = 1; i < strArray.length; i++) {
+            if (!strArray[i].toLowerCase().equals(strArray[i - 1].toLowerCase())) System.out.print(" " + strArray[i]);
+        }
+    }
+}
