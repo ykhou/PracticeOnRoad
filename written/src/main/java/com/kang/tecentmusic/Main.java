d4a62c737ad735c0a202128aeeddf0fabd61d4b7
ykhou <houyongkang@bupt.edu.cn>
2022-05-18 15:44:13 +0800

    腾讯音乐

--- written/src/main/java/com/kang/tecentmusic/Main.java
+++ written/src/main/java/com/kang/tecentmusic/Main.java
@@ -7,25 +7,25 @@ import java.util.Queue;
 
 public class Main {
     public List<List<Integer>> resList = new ArrayList<>();
-    public ArrayList<TreeNode> deleteLevel (TreeNode root, ArrayList<Integer> a) {
-        // write code here
-        if (node == null) return null;
-        Queue<TreeNode> que = new LinkedList<>();
-        que.offer(node);
-
-        while (!que.isEmpty()) {
-            List<Integer> tmpList = new ArrayList<>();
-            int len = que.size();
-
-            while (len > 0) {
-                TreeNode tmpNode = que.poll();
-                tmpList.add(tmpNode.val);
-
-                if (tmpNode.left != null) que.offer(tmpNode.left);
-                if (tmpNode.right != null) que.offer(tmpNode.right);
-                len--;
-            }
-            resList.add(tmpList);
-        }
-    }
+//    public ArrayList<TreeNode> deleteLevel (TreeNode root, ArrayList<Integer> a) {
+//        // write code here
+//        if (node == null) return null;
+//        Queue<TreeNode> que = new LinkedList<>();
+//        que.offer(node);
+//
+//        while (!que.isEmpty()) {
+//            List<Integer> tmpList = new ArrayList<>();
+//            int len = que.size();
+//
+//            while (len > 0) {
+//                TreeNode tmpNode = que.poll();
+//                tmpList.add(tmpNode.val);
+//
+//                if (tmpNode.left != null) que.offer(tmpNode.left);
+//                if (tmpNode.right != null) que.offer(tmpNode.right);
+//                len--;
+//            }
+//            resList.add(tmpList);
+//        }
+//    }
 }
